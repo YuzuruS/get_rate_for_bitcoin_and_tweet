b385e14923c985a6df3a6731260732b1b1c79a8b
YuzuruS <navitima@gmail.com>
2017-03-23 01:58:07 +0900

    update lib

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
-gem "garage_client"
+gem "rest-client"
 gem "twitter"
 gem "dotenv"
+gem "garage_client"
--- get_rate.rb
+++ get_rate.rb
@@ -1,31 +1,38 @@
-require "garage_client"
+require "rest-client"
 require "twitter"
 require "time"
 require "dotenv"
+require "garage_client"
 
-GarageClient.configure do |c|
-#  c.verbose = true
-  c.endpoint = "https://bitflyer.jp"
-  c.name = 'hoge'
+def create_text(rate_name, rate)
+  "1#{rate_name} = " + rate + "円\n"
 end
 
-client = GarageClient::Client.new(
-  path_prefix: '/api'
-)
+def tweet(text)
+  Dotenv.load File.expand_path(File.dirname($0)) + '/.env'
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["CONSUMER_KEY"]
+    config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    config.access_token        = ENV["ACCESS_TOKEN"]
+    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  end
 
-res = client.get("/echo/price")
-
-Dotenv.load File.expand_path(File.dirname($0)) + '/.env'
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  client.update(text)
 end
+
+res = RestClient.get 'https://bitflyer.jp/api/echo/price'
+res = JSON.parse(res.body)
 
 now = Time.now
-text = ">------#{now.year.to_s}/#{now.month.to_s}/#{now.day.to_s} #{now.hour.to_s}:00のレート------>\n"
-text += "1BTC = " + res.mid.to_i.to_s(:delimited) + "円\n"
-text += " #bitcoin #ビットコイン #仮想通貨"
+text = "#{now.year.to_s}/#{now.month.to_s}/#{now.day.to_s} #{now.hour.to_s}:00のレート\n"
+text += create_text 'BTC', res['mid'].to_i.to_s(:delimited)
 
-client.update(text)
+res = RestClient.get 'http://api.aoikujira.com/kawase/json/JPY'
+res = JSON.parse(res.body)
+
+%w(USD EUR GBP).each { |rate|
+  text += create_text rate, (1/res[rate].to_f).round(2).to_s
+}
+
+text += "#ビットコイン #為替レート #FX"
+tweet text
